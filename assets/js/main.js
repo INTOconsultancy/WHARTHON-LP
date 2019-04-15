99d72fdf677317d77a3f67a531f1cb3e07c6df6f
Erik Adrian Durán <daftpunkguy11@gmail.com>
2019-04-15 18:28:14 -0500

    Ajustes de landing page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,5 @@
 jQuery(document).ready(function () {
-    jQuery('.slide-testimonio').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        arrows: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    infinite: true,
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
+
     jQuery(window).scroll(function () {
         if (jQuery(this).scrollTop() < 5) {
             jQuery('.header-button').removeClass('aparecer');
@@ -48,6 +16,52 @@ jQuery(document).ready(function () {
             jQuery('.blue').addClass('aparecer');
             jQuery('.white').addClass('desaparecer');
         }
+    });
+    //If width is the resolution of the device remove class
+    if (jQuery(window).width() < 1024) {
+        jQuery('#width-detect').addClass('slide-profesores');
+        jQuery('.slide-profesores').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            arrows: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            adaptiveHeight: true,
+            responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+    }
+    else {
+        jQuery('#width-detect').removeClass('slide-profesores');
+    }
+    //Show more examples
+    jQuery("#btn-examples").click(function(){
+        jQuery(".ejemplos-ocultos").show();
+        jQuery("#btn-examples").hide();
     });
     //When click in btn "ver programas" animate to down
     jQuery(".header-button").click(function () {
