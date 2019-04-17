84d7d66f557e321f3061f5053b38d4a8103cf1dd
Erik Adrian Durán <daftpunkguy11@gmail.com>
2019-04-17 18:53:53 -0500

    Ajustes de Carpetas y GDPR

---
+++ business-analytics/Thanks.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta http-equiv="refresh" CONTENT="1;URL= https://bit.ly/2IkgWzP"> -->
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>¡Gracias por tu interés!</title>
+    <link rel="stylesheet" href="assets/scss/stylesheet.css">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/png" href="assets/img/favicon.ico" />
+    <link rel="shortcut icon" type="image/png" href="assets/img/favicon.ico" />
+
+</head>
+
+<body>
+    <div class="gracias-container">
+        <div class="dialog-container">
+            <div class="logos-container">
+                <img src="assets/img/svg/wharton-execed-blue-LG.svg" alt="WHARTON">
+            </div>
+            <div class="dialog-cont">
+                <div class="whitSBo fs24 negro19">
+                    ¡Gracias!
+                </div>
+                <div class="whitSBo fs20 negro19">
+                    Gracias por tu interés en el programa
+                </div>
+                <div class="whitMe fs16 gris70">
+                    Business Analytics: De los datos a la acción
+                </div>
+                <div class="whitMee fs16 negro46">
+                    Si tu descarga no comienza <a class=" grapBo azulB5" href="#">da click aquí</a>
+                </div>
+            </div>
+            <div class="gracias-linea"></div>
+            <div class="gracias-button">
+                <div class="robotoRe fs18 negro19">
+                    ¿Listo para el próximo paso?
+                </div>
+                <a class="btn-gracias whitSBo fs16 blanco" href="#" target="_blank" rel="noopener noreferrer">
+                    REGÍSTRATE AHORA
+                </a>
+            </div>
+            <div class="gracias-redes">
+                <div class="opsRe fs16 negro3D">
+                    Conéctate con nosotros
+                </div>
+                <div class="icon-redes">
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                        <img src="assets/img/svg/facebook-f-brands.svg" alt="Facebook">
+                    </a>
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                        <img src="assets/img/svg/twitter-brands.svg" alt="Twiter">
+                    </a>
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                        <img src="assets/img/svg/linkedin-in-brands.svg" alt="Linkedin">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
